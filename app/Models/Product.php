c67b680f01acc736ab0338018c1f924a8ecb169b
Keita <178keita1034@gmail.com>
2023-06-20 23:03:58 +0900

    product初期設定

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Shop;
+use App\Models\SecondaryCategory;
+use App\Models\Image;
 
 class Product extends Model
 {
     use HasFactory;
+
+    public function shop(){
+        return $this->belongsTo(Shop::class);
+    }
+ 
+    public function category(){
+    // メソッド名がテーブル名と異なる場合には第二引数でテーブル中の要素を指定できる
+    // 必要ならさらに、第３引数にて要素の指定も可能
+        return $this->belongsTo(SecondaryCategory::class,'secondary_category_id');
+    }
+
+    public function imageFirst(){
+        return $this->belongsTo(Image::class,'image1','id');
+    }
 }
