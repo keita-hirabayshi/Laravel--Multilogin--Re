a6a95c8f2ad400ecc7731eb893d2838fc203d5d5
Keita <178keita1034@gmail.com>
2023-07-04 21:22:36 +0900

    cart index

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -5,24 +5,45 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Cart;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
 class CartController extends Controller
 {
+    public function index(){
+        $user = User::findOrFail(Auth::id());
+        $products = $user->products;
+        $totalPrice =0;
+
+        foreach($products as $product){
+            $totalPrice += $product->price * $product->pivot->quantity;
+        }
+
+        // dd($products,$totalPrice);
+
+        return view('user.cart',
+        compact('products','totalPrice'));
+    }
+
     public function add(Request $request){
+    // cartに商品があるかをチェック。
+    // リクエストがあった、ユーザー・プロダクト情報にてすでに商品をカートに入れていれば、ここで取得される」
        $itemInCart = Cart::where('product_id',$request->product_id)
        ->where('user_id',Auth::id())->first();
 
+    // ユーザーが既に入れている場合には、追加のみされる
        if($itemInCart){
         $itemInCart->quantity += $request->quantity;
         $itemInCart->SAVE();
        }else{
+    // なければ自動生成される
            Cart::create([
                'user_id' => Auth::id(),
                'product_id' => $request->product_id,
                'quantity' => $request->quantity
            ]);
        }
-       dd('テスト');
+       
+       return redirect()->route('user.cart.index');
     }
 }
