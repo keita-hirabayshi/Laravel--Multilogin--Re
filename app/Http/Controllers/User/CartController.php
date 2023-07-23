2ff056721763fd5abaff3c4371004acf818a3842
Keita <178keita1034@gmail.com>
2023-07-23 20:18:54 +0900

    メール機能

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -8,6 +8,7 @@ use App\Models\Cart;
 use App\Models\User;
 use App\Models\Stock;
 use Illuminate\Support\Facades\Auth;
+use App\Services\CartService;
 
 class CartController extends Controller
 {
@@ -107,6 +108,11 @@ class CartController extends Controller
 
     public function checkout()
     {
+        ////
+    // ログインしているユーザーのカート情報を取得
+        $items = Cart::where('user_id',Auth::id())->get();
+        $products = CartService::getItemsInCart($items);
+        ////
         $user = User::findOrFail(Auth::id());
         $products = $user->products;
         $stripe = new \Stripe\StripeClient(env('STRIPE_SECRET_KEY'));
